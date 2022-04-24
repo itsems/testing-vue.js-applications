24f65fd1113e57b379062bc8903516c5f2e6a733
Emma <emma801006@hotmail.com>
2022-04-25 00:29:43 +0800

    chapter-7: Add vuex

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import ProgressBar from './components/ProgressBar.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import storeConfig from './store/store-config'
 
 Vue.config.productionTip = false
 
 Vue.use(VueAxios, axios)
+
+Vue.use(Vuex);
+const store = new Vuex.Store(storeConfig)
 
 const bar = new Vue(ProgressBar).$mount()
 Vue.prototype.$bar = bar
@@ -14,6 +19,7 @@ document.body.appendChild(bar.$el)
 
 new Vue({
   el: '#app',
+  store,
   render: h => h(App)
 })
 
